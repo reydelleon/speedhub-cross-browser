117e9a73b7eae164ad657fdc5bf794e6f63ceeec
Reydel Leon Machado <contact@reydelleon.me>
2015-01-08 15:24:05 -0500

    Makes chrome.storage module more generic. Now the user of the API can specify the storage type to be used when saving and retrieving data from storage.

--- js/ext/chrome.storage.js
+++ js/ext/chrome.storage.js
@@ -1,53 +1,84 @@
 /**
  * @module      chrome.storage
  * @author      Reydel Leon Machado
- * @copyright   (c) 2014 Reydel Leon-Machado
+ * @copyright   (c) 2014 Reydel Leon Machado
  * @license     Licensed under MIT license
  */
 
 if (typeof define === 'function' && define.amd) {
     define([], function () {
-        "use strict";
+            "use strict";
 
-        var localStorage = chrome.storage.local, //This is Chrome's specific API (Not the W3C specification implementation)
-            syncStorage = chrome.storage.sync, //This is Chrome's specific API (Not the W3C specification implementation)
-            set,
-            get;
+            var webStorage,
+                localStorage = chrome.storage.local, //This is Chrome's specific API (Not the W3C specification implementation)
+                syncStorage = chrome.storage.sync, //This is Chrome's specific API (Not the W3C specification implementation)
+                set,
+                get;
 
-        /**
-         * Sets multiple items.
-         * @param {Object} items An object which gives each key/value pair to update storage with. Any other key/value pairs
-         * in storage will not be affected.
-         * Primitive values such as numbers will serialize as expected. Values with a typeof "object" and "function" will
-         * typically serialize to {}, with the exception of Array (serializes as expected), Date, and Regex (serialize using
-         * their String representation).
-         * @param {function} callback Callback on success, or on failure (in which case runtime.lastError will be set).
-         *
-         * If you specify the callback parameter, it should be a function that looks like this:
-         *
-         * <tt>function() {...};</tt>
-         */
-        set = function (items, callback) {
-            localStorage.set(items, callback);
-        };
+            /**
+             * Sets multiple items.
+             * @param {Object} items An object which gives each key/value pair to update storage with. Any other key/value pairs
+             * in storage will not be affected.
+             * Primitive values such as numbers will serialize as expected. Values with a typeof "object" and "function" will
+             * typically serialize to {}, with the exception of Array (serializes as expected), Date, and Regex (serialize using
+             * their String representation).
+             * @param {function} callback Callback on success, or on failure (in which case runtime.lastError will be set).
+             *
+             * If you specify the callback parameter, it should be a function that looks like this:
+             *
+             * <tt>function() {...};</tt>
+             *
+             * @param {string} storageType Defines what type of storage will be used to store the data. Accepted values are:
+             * <tt>'web'</tt> (HTML Web Storage specification); <tt>'chrome.local'</tt> (chrome.storage.local) and
+             * <tt>'chrome.sync'</tt> (chrome.storage.sync). Standard HTML Storage specification ('web') is assumed if no value
+             * is provided.
+             */
+            set = function (items, callback, storageType) {
+                storageType = storageType || 'web';
 
-        /**
-         * Gets one or more keys from storage.
-         * @param {string | string[] | Object} keys A single key to get, list of keys to get, or a dictionary specifying default
-         * values. An empty list or object will return an empty result object. Pass in <tt>null</tt>
-         * to get the entire contents of storage.
-         * @param {function} callback Callback with storage keys, or on failure (in which case runtime.lastError will be set).
-         *
-         * The callback parameter should be a function that looks like this:
-         * <tt>function(object items) {...};</tt>
-         */
-        get = function (keys, callback) {
-            localStorage.get(keys, callback);
-        };
+                switch (storageType) {
+                    case 'chrome.local':
+                        localStorage.set(items, callback);
+                        break;
+                    default:
+                        throw new Error('The storage type provided is not valid. Accepted values are: "web", "chrome.local" and "chrome.sync"');
+                }
 
-        return {
-            set: set,
-            get: get
-        };
-    });
+            };
+
+            /**
+             * Gets one or more keys from storage.
+             * @param {string | string[] | Object} keys A single key to get, list of keys to get, or a dictionary specifying default
+             * values. An empty list or object will return an empty result object. Pass in <tt>null</tt>
+             * to get the entire contents of storage.
+             * @param {function} callback Callback with storage keys, or on failure (in which case runtime.lastError will be set).
+             *
+             * The callback parameter should be a function that looks like this:
+             * <tt>function(object items) {...};</tt>
+             *
+             * @param {string} storageType Defines from which type of storage will the data be retrieved. Accepted values are:
+             * <tt>'web'</tt> (HTML Web Storage specification); <tt>'chrome.local'</tt> (chrome.storage.local) and
+             * <tt>'chrome.sync'</tt> (chrome.storage.sync). Standard HTML Storage specification ('web') is assumed if no value
+             * is provided.
+             */
+            get = function (keys, callback, storageType) {
+                storageType = storageType || 'web';
+
+                switch (storageType) {
+                    case 'chrome.local':
+                        localStorage.get(keys, callback);
+                        break;
+                    default:
+                        throw new Error('The storage type provided is not valid. Accepted values are: "web", "chrome.local" and "chrome.sync"');
+                }
+            }
+            ;
+
+            return {
+                set: set,
+                get: get
+            };
+        }
+    )
+    ;
 }
